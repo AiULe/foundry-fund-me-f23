c39137682d0119ff20679b9a68926a0ed7e3da03
liujinxuan <1256485667@qq.com>
2023-12-23 17:25:57 +0800

    foundry-fund-me-f23 by xuan v1

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 Foundry consists of:
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+- **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
+- **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
+- **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
+- **Chisel**: Fast, utilitarian, and verbose solidity REPL.
 
 ## Documentation
 
@@ -64,3 +64,27 @@ $ forge --help
 $ anvil --help
 $ cast --help
 ```
+
+Q1:
+
+```
+Compiler run failed:
+Error (6275): Source "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol" not found: File not found. Searched the following locations: "/xuan/web3Learn/foundry-f23/foundry-fund-me-f23".
+```
+
+A1:
+pragma solidity ^0.8.19; 很怪吧，好无聊，离成功更近一点，离失败也近了一点。
+
+# TODOList
+
+1.Proper README
+2.Integeration tests
+2.1 proxychains forge install ChainAccelOrg/foundry-devops --no-commit 获取最近部署的合约地址
+3.Programatic verification
+4.Push to Github
+
+集成测试：就是测试大量的交互，测试系统的组合。
+
+Q1:
+make: \*\*\* No rule to make target 'build'. Stop.
+A1:目前解决方案 MakeFile 文件名改为 Makefile
---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user"); //创建一个测试账户
+    uint256 constant SEND_VALUE = 0.1 ether; //100000000000000000
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        // fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); //给测试账户分配假资金
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.getOwner(), msg.sender);
+    }
+
+    /**
+        What can we do work with address outside our system?
+        1. Unit
+            -Testing a specific part of our code.
+        2. Integration
+            -Testing how our code works with other parts of our code
+        3. Forked
+            -Testing our code on a simulated real enviroment
+        4. Staging
+            -Testing our code in a real enviroment that is not prod
+     */
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+
+    function testFundFailWithoutEnoughETH() public {
+        vm.expectRevert(); //the next line, should revert
+        fundMe.fund(); //send 0 value
+    }
+
+    function testFundUpdatesFundDataStructure() public {
+        vm.prank(USER); //指定调用地址
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithDrawWithASingleFunder() public funded {
+        //Arrange 安排测试，设置测试环境
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        //Act 执行测试的动作
+        uint256 gasStart = gasleft(); //gasleft()->返回交易调用中剩余的gas量
+        vm.txGasPrice(GAS_PRICE); //设置gas价格
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        uint256 gasEnd = gasleft();
+        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        console.log(gasUsed);
+
+        //Assert 进行测试断言
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank
+            //vm.deal;
+            //solidity 0.8版本以上，address不能显示转换为uint256
+            hoax(address(i), SEND_VALUE); //Q1:address(1) = ?
+            //fund the fundMe
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //between startPrank and stopPrank ,any options will sending
+        //Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        //Assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //vm.prank
+            //vm.deal;
+            //solidity 0.8版本以上，address不能显示转换为uint256
+            hoax(address(i), SEND_VALUE); //Q1:address(1) = ?
+            //fund the fundMe
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //between startPrank and stopPrank ,any options will sending
+        //Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        //Assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
+}
